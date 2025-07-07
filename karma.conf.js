0e48b0c2fe77d5dad9582a9a0c2f7908b982a704
Himanshu <modihimanshu36@gmail.com>
2025-07-07 12:45:49 +0530

    park

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
     config.set({
       frameworks: ['qunit'],
       files: [
-        { pattern: 'src/main/js/calculator.js', type: 'module' },
+        { pattern: 'my-app/src/main/js/calculator.js', type: 'module' },
         { pattern: 'src/main/myAppTests.test.js', type: 'module' }
       ],
       browsers: ['ChromeHeadless'],
